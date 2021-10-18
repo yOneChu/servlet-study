cd052e8019035b3f9c29d4d9f5ce6451018be1ac
kyhslam <kyhslam@gmail.com>
2021-10-19 00:27:31 +0900

    Model 추가

--- src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -39,9 +39,16 @@ public class FrontControllerServletV3 extends HttpServlet {
         }
 
         Map<String, String> paramMap = createParamMap(request);
-
         ModelView mv = controller.process(paramMap);
-        //view.render(request, response);
+
+        String viewName = mv.getViewName(); // 논리이름
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), request, response);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 
     private Map<String, String> createParamMap(HttpServletRequest request) {
